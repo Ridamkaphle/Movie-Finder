262c4702938d3fe1a3d19ce5e4f1aef3e16c7a99
Ridam <kaphleridam@gmail.com>
2025-02-22 00:06:05 -0600

    updated MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,32 +1,42 @@
 import "../css/MovieCard.css";
+import PropTypes from 'prop-types'; // Import PropTypes
 import { useMovieContext } from "../contexts/MovieContext";
 
-function MovieCard({movie}) {
-    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
-    const favorite = isFavorite(movie.id)
+function MovieCard({ movie }) {
+    const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
     function onFavoriteClick(e) {
-        e.preventDefault()
-        if (favorite) removeFromFavorites(movie.id)
-            else addToFavorites(movie)
+        e.preventDefault();
+        if (favorite) removeFromFavorites(movie.id);
+        else addToFavorites(movie);
     }
 
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={`https://image.omdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
-            <div className="movie-overplay">
-                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
-                    ♡
-                </button>
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                        ♥
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date?.split("-")[0]}</p>
             </div>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date?.split("-")[0]}</p>
-        </div>
-
-    </div>
-
+    );
 }
 
-export default MovieCard
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        poster_path: PropTypes.string,
+        release_date: PropTypes.string
+    }).isRequired
+};
+
+export default MovieCard;
